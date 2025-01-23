22301720de9cd6fc0e8ef9562199e22d3d3f1a35
Stanislav Červeňák <djsucho@gmail.com>
2025-01-23 11:58:52 +0100

    ADD ServiceProvider

---
+++ src/Providers/LaravelServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wame\Laravel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class LaravelServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
